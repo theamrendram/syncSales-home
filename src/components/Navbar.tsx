40e557850d805d4b4ef225a361f41faf4af0d174
KumarAmrendram <amrendramkumar@gmail.com>
2024-10-29 23:44:34 +0530

    init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import Image from "next/image"
+import { cn } from "@/lib/utils"
+import {
+    NavigationMenu,
+    // NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    // NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import logo from "@/public/syncsales_logo.png"
+import { Button } from "./ui/button"
+
+const Navbar = () => {
+    return (
+        <NavigationMenu className="bg-white shadow-lg p-2 mx-10 my-4 rounded-2xl bg-opacity-40">
+            <Image src={logo} alt="Logo" width={150} height={100} />
+            <NavigationMenuList>
+                <div className="flex gap-2">
+                    <NavigationMenuItem>
+                        <Link href="/docs" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                Documentation
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/docs" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                Documentation
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/docs" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                Documentation
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                </div>
+            </NavigationMenuList>
+            <div className="flex gap-2">
+                <Button>
+                    Login
+                </Button>
+                <Button className="w-full" >
+                    Sign Up
+                </Button>
+            </div>
+        </NavigationMenu>
+    )
+}
+
+const ListItem = React.forwardRef<
+    React.ElementRef<"a">,
+    React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+    return (
+        <li>
+            <NavigationMenuLink asChild>
+                <a
+                    ref={ref}
+                    className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        className
+                    )}
+                    {...props}
+                >
+                    <div className="text-sm font-medium leading-none">{title}</div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        {children}
+                    </p>
+                </a>
+            </NavigationMenuLink>
+        </li>
+    )
+})
+ListItem.displayName = "ListItem"
+
+export default Navbar
